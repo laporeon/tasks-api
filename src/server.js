c3fa3093844a65e6843b196022fca68f5292da81
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2021-12-06 17:35:20 -0300

    docs: create api docs with swagger

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
 
 const routes = require("./routes/routes");
 const connection = require("./database/databaseConfig");
@@ -9,8 +10,11 @@ const connection = require("./database/databaseConfig");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const swaggerFile = require("./swagger.json");
+
 app.use(cors());
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/", routes);
 
 connection
