7fab2c0458146a9e5a639fe5383b9c58d627ca9a
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2021-12-08 20:18:01 -0300

    refactor: move databaseconfig to single folder

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
+require("./database");
 
 const express = require("express");
 const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 
 const routes = require("./routes/routes");
-const connection = require("./database/databaseConfig");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,16 +17,6 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/", routes);
 
-connection
-  .authenticate()
-  .then(() => {
-    console.log("Successfully connected with database.");
-    app.emit("db-connected");
-  })
-  .catch((e) => console.log(e));
-
-app.on("db-connected", () => {
-  app.listen(port, () => {
-    console.log(`Server is running on port - ${port}`);
-  });
+app.listen(port, () => {
+  console.log(`Server is running on port - ${port}`);
 });
