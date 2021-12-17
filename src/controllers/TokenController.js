1a346a0acc1267711bf4dca7409e6e39eec3dc3b
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2021-12-17 18:34:27 -0300

    feat: init Token Controller and auth route

---
+++ src/controllers/TokenController.js
@@ -0,0 +1,44 @@
+const { compare } = require("bcrypt");
+const { sign } = require("jsonwebtoken");
+
+const User = require("../models/User");
+
+class TokenController {
+  async store(req, res) {
+    try {
+      const { username, password } = req.body;
+
+      const user = await User.findOne({ where: { username: username } });
+
+      if (!user)
+        return res.status(401).json({
+          Errors: ["Username or password incorrect."],
+        });
+
+      const passwordMatches = await compare(password, user.password);
+
+      if (!passwordMatches)
+        return res.status(401).json({
+          Errors: ["Username or password incorrect."],
+        });
+
+      const token = sign({}, process.env.SECRET, {
+        subject: user.id,
+        expiresIn: "1d",
+      });
+
+      return res.status(200).json({
+        user: {
+          username,
+        },
+        token,
+      });
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+}
+
+module.exports = new TokenController();
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,6 +6,7 @@ const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const UserController = require("../controllers/UserController");
 const TaskController = require("../controllers/TaskController");
+const TokenController = require("../controllers/TokenController");
 
 routes.get("/", UserController.list);
 routes.post("/", UserController.create);
@@ -13,5 +14,7 @@ routes.delete("/:id", UserController.delete);
 
 routes.get("/tasks", ensureAuthenticated, TaskController.list);
 routes.post("/tasks", TaskController.create);
+
+routes.post("/signin", TokenController.store);
 
 module.exports = routes;
