d0c405a94951b000a32576b6cefcc4b5adafc30e
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2021-12-23 18:06:39 -0300

    chore: create update task status method and route

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -36,6 +36,31 @@ class TaskController {
     }
   }
 
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      const task = await Task.findByPk(id);
+
+      if (!task) return res.status(404).json({ error: "Task not found." });
+
+      const { status } = req.body;
+
+      await Task.update(
+        {
+          status,
+        },
+        { where: { id } }
+      );
+
+      return res.status(200).json({ success: "Task status updated." });
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+
   async delete(req, res) {
     try {
       const { id } = req.params;
